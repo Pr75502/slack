dd242cf8e94d5f56114bcd201c84d8d888899f30
pr75502 <pp1614582@gmail.com>
2026-02-13 03:17:00 +0530

    hello

--- src/firebase.js
+++ src/firebase.js
@@ -1,24 +1,21 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDPzfSl0rXAeoMuXtmlKjIuz3lzpYijSPs",
-    authDomain: "slackclone-15f43.firebaseapp.com",
-    projectId: "slackclone-15f43",
-    storageBucket: "slackclone-15f43.firebasestorage.app",
-    messagingSenderId: "1079951153531",
-    appId: "1:1079951153531:web:0df116ac79bf85638b85de",
+  apiKey: "AIzaSyDNmivRzHCFSzkBU9A_5H9Xq0X4y0_OTEs",
+  authDomain: "slackclone-v2.firebaseapp.com",
+  projectId: "slackclone-v2",
+  storageBucket: "slackclone-v2.firebasestorage.app",
+  messagingSenderId: "810276678422",
+  appId: "1:810276678422:web:17e3d9340d5fbd3fbf7e37",
+  measurementId: "G-BG57SSM8VF"
 };
 
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-
-
-
-
-
-
-
